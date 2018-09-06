00cbdfad5171d28f696090456318cfa1a90205e1
Ievgeniia Svetsynska <ievgeniia.svetsynska@tu-dresden.de>
2018-09-06 20:53:48 +0200

    Add comments

--- main-node/tools/splitter.py
+++ main-node/tools/splitter.py
@@ -1,13 +1,14 @@
 import csv
 
+=
 class Splitter:
     data = []
     new_data = []
-    param_list = {'app': "app4", 'flac': "cr_audio1.flac", 'wav': "cr_audio1.wav",
-                                          'enw8': "enwik8", 'enw9': "enwik9", 'game': "game1",
-                                          '01': "01",'02': "02",'03': "03",'04': "04",'05': "05",'06': "06",'07': "07",
-                                          '08': "08",'09': "09",'10': "10",'11': "11",'12': "12",'13': "13",'14': "14",
-                                          '16': "16",'17': "17",'18': "18",'19': "19",'20': "20",'21': "21",'22': "22", 'sort':"sort", 'encrypt':"encrypt", 'decrypt':"decrypt"}
+    param_list = {'app': "app4", 'flac': "cr_audio1.flac", 'wav': "cr_audio1.wav", 'enw8': "enwik8", 'enw9': "enwik9",
+                  'game': "game1", '01': "01",'02': "02",'03': "03",'04': "04",'05': "05",'06': "06",'07': "07",
+                  '08': "08",'09': "09",'10': "10",'11': "11",'12': "12",'13': "13",'14': "14", '16': "16",'17': "17",
+                  '18': "18",'19': "19",'20': "20",'21': "21",'22': "22", 'sort':"sort", 'encrypt':"encrypt",
+                  'decrypt':"decrypt"}
 
     def __init__(self, file_name):
         del self.data[:]
